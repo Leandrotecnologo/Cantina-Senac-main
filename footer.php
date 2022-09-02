b2534db188905a0fae41d9a11468082cca220727
Leandro <leandro.lobo@alunos.sc.senac.br>
2022-09-02 15:22:20 -0300

    leandrando

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer style="margin-block-end: 300px;" class="rodape">
+
+
+        <img class="rodape-imagem" src="site/images/logosemfundo.png" alt="">
+
+        <div style=" padding: 32px; display: flex; flex-direction: row; justify-content: space-around; align-items: center;"; class="margin">
+
+            <ul>
+
+                <a href="https://www.instagram.com/cantinasenac/"><img style=" max-width: 40px;" src="site/images/insta.png" alt=""></a>
+
+            </ul>
+
+            <ul>
+
+                <a href="https://twitter.com/CantinaSenac"><img style=" max-width: 40px;" src="site/images/passaro.png"></a>
+
+            </ul>
+
+            <ul>
+
+                <a href="https://www.facebook.com/cantinasenac/"><img style=" max-width: 40px; margin: ; " src="site/images/face.png"></a>
+
+            </ul>
+
+        </div>
+
+    </footer>
+    
+
+
+
+
+
